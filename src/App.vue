4c0711de3c12102a8b0a54f87b220ff051a2d08e
Ramón Diaz <ramon.diazguerra@gmail.com>
2022-09-28 23:27:20 +0200

    bio completo

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import MyBio from './assets/components/MyBio.vue';
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<MyBio
+name="Ramon Diaz"
+age="31"
+birthplace="Santander"
+currentLocation="Barcelona"
+/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
---
+++ src/assets/components/MyBio.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>{{name}}</h1>
+    <h2>{{age}}</h2>
+    <h3>{{birthplace}}</h3>
+    <h4>{{currentLocation}}</h4>
+    <hr/>
+    <NameComp :name="name" />
+    <AgeComp :age="age" />
+    <BirthplaceComp :birthplace="birthplace"/>
+    <CurrentLocationComp :currentLocation="currentLocation" />
+  
+</template>
+
+<script setup>
+    import {ref} from 'vue';
+
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+    },
+    birthplace:{
+        type: String,
+        default: "World",
+    },
+    currentLocation: {
+        type: String,
+    },
+});
+
+</script>
+
+<style>
+
+</style>
